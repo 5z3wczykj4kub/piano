a1c8e701e5606eb3dbb4eb7d3ffc577770708abb
5z3wczykj4kub <5z3wczykj4kub@gmail.com>
2021-04-20 22:41:30 +0200

    add keyboard support

--- main.js
+++ main.js
@@ -1,30 +1,49 @@
 const whiteKeys = document.querySelectorAll('.white-key');
 const blackKeys = document.querySelectorAll('.black-key');
+const sounds = document.querySelectorAll('audio');
 
 whiteKeys.forEach(key => {
     key.addEventListener('click', event => {
         event.stopPropagation();
-        key.lastElementChild.currentTime = 0;
-        key.lastElementChild.play();
+        key.firstElementChild.currentTime = 0;
+        key.firstElementChild.play();
+    })
 
+    key.firstElementChild.addEventListener('play', () => {
         key.classList.add('white-key--active');
-
-        key.lastElementChild.addEventListener('ended', () => {
-            key.classList.remove('white-key--active');
-        })
+    })
+    key.firstElementChild.addEventListener('ended', () => {
+        key.classList.remove('white-key--active');
     })
 })
 
 blackKeys.forEach(key => {
     key.addEventListener('click', event => {
         event.stopPropagation();
-        key.lastElementChild.currentTime = 0;
-        key.lastElementChild.play();
-
-        key.classList.add('black-key--active');
-
-        key.lastElementChild.addEventListener('ended', () => {
-            key.classList.remove('black-key--active');
-        })
+        key.firstElementChild.currentTime = 0;
+        key.firstElementChild.play();
     })
+
+    key.firstElementChild.addEventListener('play', () => {
+        key.classList.add('black-key--active');
+    })
+    key.firstElementChild.addEventListener('ended', () => {
+        key.classList.remove('black-key--active');
+    })
+})
+
+const keyboardKeys = ['z', 's', 'x', 'd', 'c', 'v', 'g', 'b', 'h', 'n', 'j', 'm'];
+const keyboardKeysForWhitePianoKeys = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const keyboardKeysForBlackPianoKeys = ['s', 'd', 'g', 'h', 'j'];
+
+document.addEventListener('keydown', event => {
+    if(keyboardKeys.includes(event.key)) {
+        if(event.repeat) return;
+
+        const indexOfPressedKeyboardKey = keyboardKeys.indexOf(event.key);
+        const sound = sounds[indexOfPressedKeyboardKey];
+
+        sound.currentTime = 0;
+        sound.play();
+    }
 })
